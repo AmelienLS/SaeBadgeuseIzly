29c53e9029163ce21fe81506b5edfd89cd067287
Amelienls <laradesimiamelien@gmail.com>
2025-11-14 16:16:45 +0100

    moves

--- src/board_init.h
+++
@@ -1,9 +0,0 @@
-#ifndef BOARD_INIT_H
-#define BOARD_INIT_H
-
-// Function prototypes for board initialization
-void initializeBoard();
-void configurePeripherals();
-void boardInit();
-
-#endif // BOARD_INIT_H
